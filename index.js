281c21c96253432b2f0665f9db8e3f2e4aad8d19
salmantarik-dot <salman.tariq.reader@gmail.com>
2025-07-31 10:16:02 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Chat with ChatGPT</title>
+</head>
+<body>
+  <h1>ChatGPT Web</h1>
+  <form id="chat-form">
+    <input type="text" id="user_input" placeholder="Ask anything..." required style="width: 300px;">
+    <button type="submit">Send</button>
+  </form>
+  <div id="response"></div>
+
+  <script src="index.js"></script>
+</body>
+</html>
